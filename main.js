3341c7ed2559cd9368ac86540f32d013aa87fcee
Pietro Klein <117283920+pietrokp@users.noreply.github.com>
2023-01-31 22:30:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function createGame(player1, hour, player2) {
     return `
         <li>
-            <img src="./assets/icon-${player1}.svg" alt="">
+            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
             <strong>${hour}</strong>
             <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
         </li>
